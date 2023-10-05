f852a3e7212de3e65b586e394208691ad1ac2717
Nikita Klishin <n.klishin@oblakogroup.ru>
2023-10-05 15:30:13 +0300

    MVP 0.0.10

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -18,7 +18,6 @@ async function bootstrap() {
 
     app.enableCors({
         allowedHeaders: "*",
-        origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
         credentials: true,
     })
     // app.useGlobalPipes(new ValidationPipe())
