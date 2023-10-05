7c2b5114f34ce0fb19399b60798ca4ed3ea3bfc7
Nikita Klishin <n.klishin@oblakogroup.ru>
2023-10-05 13:22:53 +0300

    MVP 0.0.3

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -15,6 +15,8 @@ import {Brand} from "./brands/entities/brand.entity";
 import {Category} from "./categories/entities/category.entity";
 import {Device} from "./devices/entities/device.entity";
 import {Role} from "./roles/entities/role.entity";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import * as Joi from 'joi';
 
 const entity_modules = [
   BasketsModule,
@@ -29,15 +31,33 @@ const entity_modules = [
   imports: [
       AuthModule,
     ...entity_modules,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'online_store',
-      entities: [Basket, Brand, Category, Device, Role, User],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+            .valid('development', 'production', 'test', 'provision')
+            .default('development'),
+        PORT: Joi.number().default(4000),
+        DB_USER: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string(),
+        DB_NAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required()
+      })
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT', undefined),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [Basket, Brand, Category, Device, Role, User],
+        synchronize: true,
+        ssl: true
+      }),
+      inject: [ConfigService]
     }),
   ],
   controllers: [AppController],
